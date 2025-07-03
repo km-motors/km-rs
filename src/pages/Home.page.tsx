bafd1c0e5a8e3b85ab11faf38d79715cd062dbc6
KM-RS1 <caryardmkmotors@gmail.com>
2025-07-03 16:25:56 -0400

    fixed floating menu hides

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,21 +1,25 @@
 import { Box, Stack, Center, Group } from '@mantine/core';
 
-import { FloatingMenu } from '@/components/FloatingMenu';
+import { FloatingMenu, PAGES } from '@/components/FloatingMenu';
 import { OptionsMenu } from '@/components/OptinsMenu';
 import { UserMenu } from '@/components/UserMenu';
 import { Forms } from '@/components/Forms';
+import { IncomeList } from '@/components/IncomeTab';
+import { useLocalStorage } from '@mantine/hooks';
 
 export function HomePage() {
-
+  const [currentPage] = useLocalStorage({ key: "--current-page" });
 
   return (
-    <Stack w="100vw" h="100vh" bg={"var(--mantine-primary-color-0)"} pb="xl" px="0" pt="0" gap="xl">
+    <Stack w="100vw" h="100vh" bg={"var(--mantine-primary-color-0)"} pb="xl" px="0" pt="0" gap="0">
       <Group justify="space-between">
         <OptionsMenu />
         <UserMenu />
       </Group>
-      <Box className='data-block' flex={1} />
-      <Center>
+      <Box className='data-block' flex={1} style={{ overflow: "scroll", borderTop: "1px solid #bbb" }}>
+        {currentPage == PAGES.INCOME && <IncomeList />}
+      </Box>
+      <Center style={{ position: "fixed", bottom: "0", left: "50%", translate: "-50% 0" }} mb={"lg"}>
         <FloatingMenu />
       </Center>
       <Forms />
