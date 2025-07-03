b1480675ecbb1bacd1b9889dbb973eac9a72719e
KM-RS1 <caryardmkmotors@gmail.com>
2025-07-03 12:39:17 -0400

    user-menu: logout button

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -1,11 +1,29 @@
-import { ActionIcon } from "@mantine/core";
+import { ActionIcon, Menu } from "@mantine/core";
 import { iconProps } from "./FloatingMenu";
-import { ReactComponent as UserIcon } from "@/icons/user(1).svg?react";
+import { ReactComponent as IconUser } from "@/icons/user(1).svg?react";
+import { ReactComponent as IconLogout } from "@/icons/logout.svg?react";
+import { useNavigate } from "react-router-dom";
+import { supabase } from "@/lib/supabaseClient";
 
 export function UserMenu() {
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        await supabase.auth.signOut();
+        navigate('/');
+    };
     return (
-        <ActionIcon variant="transparent" size={60}>
-            <UserIcon {...iconProps} style={{ strokeWidth: 1.5 }} />
-        </ActionIcon>
+        <Menu shadow="md" width={200}>
+            <Menu.Target>
+                <ActionIcon variant="transparent" size={60}>
+                    <IconUser {...iconProps} style={{ strokeWidth: 1.5 }} />
+                </ActionIcon>
+            </Menu.Target>
+            <Menu.Dropdown>
+                <Menu.Item color="red" leftSection={<IconLogout width={14} height={14} />} onClick={handleLogout}>
+                    Logout
+                </Menu.Item>
+            </Menu.Dropdown>
+        </Menu>
     )
 }
