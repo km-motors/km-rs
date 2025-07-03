6ff7cf4676fc12994ceb3a86abefd494d07e1c1f
KM-RS1 <caryardmkmotors@gmail.com>
2025-07-03 12:39:45 -0400

    usermneu : logout button

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,17 +1,11 @@
-import { Button, Container, Title, Box, ActionIcon, Flex, Stack, Center, Group } from '@mantine/core';
-import { supabase } from '../lib/supabaseClient';
-import { useNavigate } from 'react-router-dom';
+import { Box, Stack, Center, Group } from '@mantine/core';
+
 import { FloatingMenu } from '@/components/FloatingMenu';
 import { OptionsMenu } from '@/components/OptinsMenu';
 import { UserMenu } from '@/components/UserMenu';
 
 export function HomePage() {
-  const navigate = useNavigate();
-
-  const handleLogout = async () => {
-    await supabase.auth.signOut();
-    navigate('/');
-  };
+  
 
   return (
     <Stack w="100vw" h="100vh" bg={"var(--mantine-primary-color-0)"} pb="xl" px="0" pt="0" gap="xl">
